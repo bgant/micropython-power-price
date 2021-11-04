8703cbb1f8df70ff6993a2efb8ff7a5ed4700000
Brandon Gant <bgant@protonmail.com>
2021-11-04 11:35:24 -0500

    psp.check_date() works

--- psp_json.py
+++ psp_json.py
@@ -29,30 +29,27 @@ import urequests
 # Download Power Smart Pricing data in JSON via API
 def download():
     url = 'https://www.ameren.com/api/ameren/promotion/RtpHourlyPricesbyDate'
-    date = {"SelectedDate":"2021-11-03"}
+    date = {"SelectedDate":date_today()}
     headers = {'User-Agent': 'https://github.com/bgant/micropython-power-price'}
     response = urequests.post(url, headers=headers, json=date)
-    if response.status_code == '200':
-        raw_data = response.json()
+    if response.status_code is 200:
+        data = response.json()
         print(f'{timestamp()} New JSON data downloaded... Waiting one minute before continuing...')
-        return raw_data
     else:
         print(f'{timestamp()} Download failed with HTTP Status Code {response.status_code}... Not sure what to do...')
     response.close()
     time.sleep(65)
+    return data
 
 # Check that JSON data is for today
 def check_date():
-    try: 
-        date_json = raw_data['hourlyPriceDetails'][0]['date'].strip('T00:00:00')   # YYYY-MM-DD
-    except:
+    global raw_data
+    date_json = raw_data['hourlyPriceDetails'][0]['date'].strip('T00:00:00')   # YYYY-MM-DD
+    if date_json != date_today():
         raw_data = download()
-    date_today = f'{time.localtime(tz())[0]}-{time.localtime(tz())[1]}-{time.localtime(tz())[2]}'
-
-    if date_json != date_today:
-        raw_data = download()
-    elif date_json == date_today:
-        print(f"{timestamp()} JSON data today's date ({date_today})")
+        reset()
+    elif date_json == date_today():
+        print(f"{timestamp()} JSON data today's date ({date_today()})")
     else:
         print(f'{timestamp()} Something went wrong... Exiting...')
         exit()
@@ -72,13 +69,14 @@ def parse():
     #print(today)
     return today
 
+def date_today():
+    return f'{time.localtime(tz())[0]}-{time.localtime(tz())[1]:02}-{time.localtime(tz())[2]:02}'
+
 # Timestamp for debugging
 def timestamp():
     return f'[{time.localtime(tz())[3]:02}:{time.localtime(tz())[4]:02}:{time.localtime(tz())[5]:02}]'
 
-
 raw_data = download()
-
 
 #r.json()
 #r.json()['isNextDay']
